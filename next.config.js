74c72c2ac519fd5dff1dd13f8187698a7245ce68
Agung Kurniawan <agung.kurniawan@sehatq.com>
2022-05-05 14:30:49 +0800

    - fixed next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['media.graphcms.com'],
+    domains: ['media.graphcms.com', 'media.graphassets.com'],
   },
   publicRuntimeConfig: {
     API: process.env.BASE_API,
